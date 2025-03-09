b8b9bbde9c1da3ae44b4c6e52ff76340191be9e8
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2025-03-09 13:40:13 -0400

    run build added dist and start-all.bet

--- config/db.js
+++ config/db.js
@@ -4,7 +4,6 @@ const connectDB = async () => {
   try {
     await mongoose.connect('mongodb://localhost:27017/kmInventoryDB', {
       useNewUrlParser: true,
-      useUnidiedTopology: true,
     });
     console.log('Connected to MongoDB');
   } catch (err) {
